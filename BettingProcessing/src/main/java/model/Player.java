76771e6c325b8f374aad685bea43539e26971d2b
Taeri <taesaa@ttu.ee>
2023-11-14 17:14:35 +0200

    rename

---
+++ BettingProcessing/src/main/java/model/Player.java
@@ -0,0 +1,15 @@
+package model;
+
+import lombok.*;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class Player {
+    @NonNull
+    private UUID id;
+    private int balance = 0;
+}
